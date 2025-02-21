87d57428c691ce2bcd328e36062b44fe802a62d1
JulioDiz17 <julio@LAPTOP-F7LAKEI0.home>
2025-02-21 17:35:25 +0100

    Añadida clase MensajesRama1

---
+++ Proyecto2/src/Mensaje/MensajeRama1.java
@@ -0,0 +1,10 @@
+package Mensaje;
+
+public class MensajeRama1 {
+
+	public static void main(String[] args) {
+		System.out.println("Mensaje 1 desde rama1");
+        System.out.println("Mensaje 2 desde rama1");
+	}
+
+}
